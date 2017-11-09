26a93c63c155589108ef4bca60d15df05b52574e
miryalaharshavardhan <miryalaharshavardhan4@gmail.com>
2017-11-09 13:27:00 +0530

    added testinput

--- krushkal/krushkal.cpp
+++ krushkal/krushkal.cpp
@@ -228,7 +228,7 @@ void display()
 	        }
 	    }
 	}
-    glglColor3f(1.0f, 1.0f, 1.0f);
+    glColor3f(1.0f, 1.0f, 1.0f);
     str = "-----------------------------------------------------------";
     drawstr(10,90, str.c_str(), str.length());
     str = "White - Initial color of the node";
